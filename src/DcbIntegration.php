faee80b7aa3b89f3e49258c6715a38d1744b2907
Emmanuel Mnzava <epmnzava@gmail.com>
2025-11-17 15:31:45 +0300

    first initial commit

--- src/DcbIntegration.php
+++ src/DcbIntegration.php
@@ -2,7 +2,293 @@
 
 namespace Poagas\DcbIntegration;
 
+use Carbon\Carbon;
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Request;
+
 class DcbIntegration
 {
-    // Build your next great package.
+
+
+  protected $base_url;
+  protected $client_id;
+  protected $client_secret;
+
+
+  public function __construct($base_url, $client_id, $client_secret)
+  {
+    $this->base_url = $base_url;
+    $this->client_id = $client_id;
+    $this->client_secret = $client_secret;
+  }
+
+  public function create_token()
+  {
+
+
+    $client = new Client;
+    $headers = [
+      'Content-Type' => 'application/json'
+    ];
+    $body = '{
+  "clientId": "' . $this->client_id . '",
+  "clientSecret": "' . $this->client_secret . '"
+}';
+    $request = new Request('POST', $this->base_url . '/koinetPay/client/v3/authentication/token', $headers, $body);
+    $res = $client->sendAsync($request)->wait();
+
+    return json_encode($res->getBody());
+  }
+
+
+  public function query_fsp($token)
+  {
+
+
+    $client = new Client();
+    $headers = [
+      'datetime' => Carbon::now(),
+      'Content-Type' => 'application/json',
+      'Authorization' => 'Bearer ' . $token,
+    ];
+    $request = new Request('GET', $this->base_url . '/koinetPay/aggregator/v3/queryTipsFsps', $headers);
+    $res = $client->sendAsync($request)->wait();
+    return json_encode($res->getBody());
+  }
+
+  public function account_lookup($token, $accountNo, $institutionCode)
+  {
+
+    $client = new Client();
+    $headers = [
+      'Content-Type' => 'application/json',
+      'Authorization' => 'Bearer ' . $token
+    ];
+    $body = '{
+  "accountNo": "' . $accountNo . '",
+  "institutionCode": "' . $institutionCode . '"
+}';
+    $request = new Request('POST', $this->base_url . '/koinetPay/aggregator/v3/accountLookup', $headers, $body);
+    $res = $client->sendAsync($request)->wait();
+    return json_encode($res->getBody());
+  }
+
+
+
+  public function merchant_lookup($token, $tillNumber, $institutionCode)
+  {
+
+    $client = new Client();
+    $headers = [
+      'Content-Type' => 'application/json',
+      'Authorization' => 'Bearer ' . $token
+    ];
+    $body = '{
+  "tillNumber": "' . $tillNumber . '",
+  "institutionCode": "' . $institutionCode . '"
+}';
+    $request = new Request('POST', $this->base_url . '/koinetPay/tips/v3/merchantLookup', $headers, $body);
+    $res = $client->sendAsync($request)->wait();
+    return json_encode($res->getBody());
+  }
+
+
+  public function wallet_transfer($token, $payload)
+  {
+    $client = new Client();
+    $headers = [
+      'Content-Type' => 'application/json',
+      'Authorization' => 'Bearer ' . $token
+    ];
+    $body = '{
+  "sourceAccount": "' . $payload['sourceAccount'] . '",
+  "senderName": "' . $payload['senderName'] . '",
+  "msisdn": "' . $payload['msisdn'] . '",
+  "destinationAccount": "' . $payload['destinationAccount'] . '",
+  "amount": "' . $payload['amount'] . '",
+  "purpose": "' . $payload['purpose'] . '",
+  "beneficiaryName": "' . $payload['beneficiaryName'] . '",
+  "institutionCode": "' . $payload['institutionCode'] . '",
+  "reference": "' . $payload['reference'] . '",
+  "callbackUrl": "' . $payload['callbackUrl'] . '"
+}';
+    $request = new Request('POST', $this->base_url . '/koinetPay/aggregator/v3/transfers', $headers, $body);
+    $res = $client->sendAsync($request)->wait();
+    return json_encode($res->getBody());
+  }
+
+
+  public function send_auth_token($token, $trans_token, $reference)
+  {
+    $client = new Client();
+    $headers = [
+      'Content-Type' => 'application/json',
+      'Authorization' => 'Bearer ' . $token
+    ];
+    $body = '{
+  "token": "' . $trans_token . '",
+  "reference": "' . $reference . '"
+}';
+    $request = new Request('POST', $this->base_url . '/koinetPay/aggregator/v3/paymentAuthorization', $headers, $body);
+    $res = $client->sendAsync($request)->wait();
+    return json_encode($res->getBody());
+  }
+
+
+  public function merchant_payment($token, $payload)
+  {
+
+    $client = new Client();
+    $headers = [
+      'Content-Type' => 'application/json',
+      'Authorization' => 'Bearer ' . $token
+    ];
+
+    $body = '{
+  "sourceAccount": "' . $payload["sourceAccount"] . '",
+  "senderName": "' . $payload["senderName"] . '",
+  "customerPhone": "' . $payload["customerPhone"] . '",
+  "tillNo": "' . $payload["tillNo"] . '",
+  "amount": "' . $payload["amount"] . '",
+  "purpose": "' . $payload["purpose"] . '",
+  "beneficiaryName": "' . $payload["beneficiaryName"] . '",
+  "merchantCode": "' . $payload["merchantCode"] . '",
+  "reference": "' . $payload["reference"] . '",
+  "callbackUrl": "' . $payload["callbackUrl"] . '"
+}';
+    $request = new Request('POST', $this->base_url . '/koinetPay/aggregator/v3/merchantPayments', $headers, $body);
+    $res = $client->sendAsync($request)->wait();
+
+    return json_encode($res->getBody());
+  }
+
+
+  public function bill_payments($token, $payload)
+  {
+    $client = new Client();
+    $headers = [
+      'Content-Type' => 'application/json',
+      'Authorization' => 'Bearer ' . $token
+    ];
+    $body = '{
+  "sourceAccount": "' . $payload['sourceAccount'] . '",
+  "customerNo": "' . $payload['customerNo'] . '",
+  "senderName": "' . $payload['senderName'] . '",
+  "msisdn": "' . $payload['msisdn'] . '",
+  "smartCard": "' . $payload['smartCard'] . '",
+  "amount": "' . $payload['amount'] . '",
+  "purpose": "' . $payload['purpose'] . '",
+  "beneficiaryName": "' . $payload['beneficiaryName'] . '",
+  "billerCode": "' . $payload['billerCode'] . '",
+  "description": "' . $payload['description'] . '",
+  "reference": "' . $payload['reference'] . '",
+  "callbackUrl": "' . $payload['callbackUrl'] . '"
+}';
+    $request = new Request('POST', $this->base_url . '/koinetPay/aggregator/v3/billerPayment', $headers, $body);
+    $res = $client->sendAsync($request)->wait();
+
+
+    return json_encode($res->getBody());
+  }
+
+  public function bill_lookup($token,$smartCardNumber,$reference,$billerCode){
+        $client = new Client();
+    $headers = [
+      'Content-Type' => 'application/json',
+      'Authorization' => 'Bearer ' . $token
+    ];
+
+$body = '{
+  "smartCardNumber": "'.$smartCardNumber.'",
+  "reference": "'.$reference.'",
+  "billerCode": "'.$billerCode.'"
+}';
+$request = new Request('POST', $this->base_url.'/koinetPay/aggregator/v3/billersLookup', $headers, $body);
+      $res = $client->sendAsync($request)->wait();
+
+
+    return json_encode($res->getBody());
+  }
+
+  public function gepg_lookup($token,$controlNo,$amount,$currency){
+
+     $client = new Client();
+    $headers = [
+      'Content-Type' => 'application/json',
+      'Authorization' => 'Bearer ' . $token
+    ];
+
+    $body = '{
+  "controlNo": "'.$controlNo.'",
+  "amount": "'.$amount.'",
+  "currency": "'.$currency.'"
+}';
+$request = new Request('POST', $this->base_url.'/koinetPay/aggregator/v3/gepgLookup', $headers, $body);
+$res = $client->sendAsync($request)->wait();
+
+    return json_encode($res->getBody());
+  }
+  public function biller_codes(){
+    return '{{
+    "id": 1,
+    "Biller Code": "DSTV",
+    "Biller Description": "DSTV subscriptions"
+  },
+  {
+    "id": 2,
+    "Biller Code": "DSTVBO",
+    "Biller Description": "DSTV Box office subscriptions"
+  },
+  {
+    "id": 3,
+    "Biller Code": "AZAMTV",
+    "Biller Description": "AZAMTV subscriptions"
+  },
+  {
+    "id": 4,
+    "Biller Code": "STARTIMES",
+    "Biller Description": "STARTIMES subscriptions"
+  },
+  {
+    "id": 5,
+    "Biller Code": "ZUKU",
+    "Biller Description": "ZUKU subscriptions"
+  },
+  {
+    "id": 6,
+    "Biller Code": "SMILE",
+    "Biller Description": "SMILE 4G internet"
+  },
+  {
+    "id": 7,
+    "Biller Code": "ZUKUFIBER",
+    "Biller Description": "ZUKU fiber internet"
+  },
+  {
+    "id": 8,
+    "Biller Code": "TTCL",
+    "Biller Description": "TTCL Prepaid and broadband"
+  },
+  {
+    "id": 10,
+    "Biller Code": "GePG",
+    "Biller Description": "Government payments"
+  },
+  {
+    "id": 11,
+    "Biller Code": "PW",
+    "Biller Description": "Precision air payments"
+  },
+  {
+    "id": 12,
+    "Biller Code": "LUKU",
+    "Biller Description": "Prepaid electricity"
+  },
+  {
+    "id": 13,
+    "Biller Code": "TOP",
+    "Biller Description": "Prepaid airtimes"
+  }
+
+  }';
 }
